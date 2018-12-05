a14bbd5eef7b13cd15aa9d37fe8111b68178e57f
lifesailor <sailyourlife91@gmail.com>
2018-12-05 22:09:23 +0900

    data

--- data.py
+++ data.py
@@ -2,6 +2,34 @@ import os
 import pydicom
 
 
+def load_images(args, logger):
+
+    base_path = os.path.abspath(args.data)
+    original_path = args.original
+    contrast_path = args.contrast
+
+    files = os.listdir(base_path)
+    files.sort()
+    files = [file for file in files if '_' not in file and ' ' not in file]
+    original_total_path = [os.path.join(os.path.join(base_path, file), original_path) for file in files]
+    contrast_total_path = [os.path.join(os.path.join(base_path, file), contrast_path) for file in files]
+
+    logger.info("LOAD DICOM FILES")
+    original_total_files = load_files(original_total_path)
+    contrast_total_files = load_files(contrast_total_path)
+
+    logger.info("SELECT FILE BY MANUFACTURER")
+    original_files = get_manufacturer_files(args.manufacturer, original_total_files)
+    contrast_files = get_manufacturer_files(args.manufacturer, contrast_total_files)
+    logger.info("GE: %d " % len(original_files))
+    logger.info("SIEMNES: %d " % len(contrast_files))
+
+    logger.info("GET IMAGES")
+    original_images = get_images(original_files)
+    contrast_images = get_images(contrast_files)
+    return original_images, contrast_images
+
+
 def load_files(path):
     total_files = []
 
@@ -19,7 +47,7 @@ def load_files(path):
     return total_files
 
 
-def load_images(files):
+def get_images(files):
     total_images = []
 
     # folder
@@ -31,3 +59,27 @@ def load_images(files):
             sequence_image.append(sequence.pixel_array)
         total_images.append(sequence_image)
     return total_images
+
+
+def get_manufacturer_files(manufacturer, files):
+    manufacturer_files = []
+
+    if manufacturer == 'all':
+        return files
+    else:
+        for file in files:
+            if manufacturer == check_manufacturer(file):
+                manufacturer_files.append(file)
+        return manufacturer_files
+
+
+def check_manufacturer(file):
+    file_info = file[0]
+    file_info = str(file_info)
+    manufacturer = file_info.split('Manufacturer')[1]
+    manufacturer = manufacturer.split('\n')[0].split('LO:')[1].lower()
+
+    if 'ge' in manufacturer:
+        return 'ge'
+    else:
+        return 'siemens'
